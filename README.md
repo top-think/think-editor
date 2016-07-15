50f54baf22630c35a33e01bef44964fab20c328d
chunice <hrb@usa.com>
2016-07-15 15:55:51 +0800

    添加ueditor

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # thinkphp5 TagLib 编辑器
 
 > 更新完善中
+
+必须在页面中先引入 TagLib
+~~~
+<!-- 声明使用 TagLib -->
+{taglib name="Editor" /}
+<!-- 引入 ueditor 使用的静态资源文件 type:编辑器名称，path:编辑器静态资源存放路径 -->
+{Editor:assets type="ueditor" path="https://assets.thinkphp.cn/ueditor/" /}
+~~~
+
+在需要使用 ueditor 编辑器的地方插入
+~~~
+{Editor:ueditor name="content" id="container"}{$item.content}{/Editor:ueditor}
+~~~
--- src/Editor.php
+++ src/Editor.php
@@ -10,9 +10,59 @@
 // +----------------------------------------------------------------------
 namespace think\template\taglib;
 
+use think\Request;
 use think\template\TagLib;
 
 class Editor extends TagLib
 {
+    // 标签定义
+    protected $tags = [
+        // 标签定义： attr 属性列表 close 是否闭合（0 或者1 默认1） alias 标签别名 level 嵌套层次
+        'assets' => ['attr' => 'type,src', 'close' => 0],
+        'ueditor' => ['attr' => 'name,id,style', 'close' => 1],
+    ];
 
+    /**
+     * 引入静态资源标签
+     * 格式：
+     * {php}echo $name{/php}
+     * @access public
+     * @param array $tag 标签属性
+     * @return string
+     */
+    public function tagAssets($tag)
+    {
+        $parseStr = '';
+        $src = isset($tag['src']) ? $tag['src'] : Request::instance()->baseUrl() . 'static/ueditor/';
+
+        switch ($tag['type']) {
+            case 'ueditor':
+                $parseStr .= "<!-- 配置文件 --><script type=\"text/javascript\" src=\"{$src}ueditor.config.js\"></script>";
+                $parseStr .= "<!-- 编辑器源码文件 --><script type=\"text/javascript\" src=\"{$src}ueditor.all.min.js\"></script>";
+                break;
+        }
+
+        return $parseStr;
+    }
+
+    /**
+     * ueditor 编辑器
+     * 格式：
+     * {Editor:ueditor name="contentt" id="container" style="color:red;"}{/Editor:ueditor}
+     * @access public
+     * @param array $tag 标签属性
+     * @param array $content 标签内容
+     * @return string
+     */
+    public function tagUeditor($tag, $content)
+    {
+        $name = $tag['name'];
+        $id = isset($tag['id']) ? $tag['id'] : 'container';
+        $style = isset($tag['style']) ? $tag['style'] : '';
+
+        $parseStr = "<!-- 加载编辑器的容器 --><script id=\"{$id}\" name=\"{$name}\" type=\"text/plain\" style=\"{$style}\">{$content}</script>";
+        $parseStr .= "<!-- 实例化编辑器 --><script type=\"text/javascript\">var ue_{$id} = UE.getEditor('{$id}');</script>";
+
+        return $parseStr;
+    }
 }
